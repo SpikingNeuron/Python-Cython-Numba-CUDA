462d62c58e3b8601cf05cd99e76b86e1ca031e1d
neuron-git-desktop <praveenneuron@gmail.com>
2016-03-15 21:52:10 +0100

    adding the files

--- README.md
+++ README.md
@@ -4,6 +4,32 @@
 
 ## About Project
 
+Python is very slow. To improve the speed we can use Cython. Also most libraries that come with Python are implemented using Cython for performance. 
+
+When it comes to GPU programming CUDA has good support with C++. 
+
+But with Python we have only two options as below.
+
++ PyCUDA
+    + Open source
+    + Needs to write kernels in string which is not elegant
++ Continuum accelerate library (previously known as numbapro)
+    + Paid version
+    + Full integration with Python
+    
+Third option we have is use C++ CUDA with Cython.
+
+Advantages
+
++ Development in pure C++ for CUDA code so all IDE functionality is available.
++ Compile Cython code to use the C++ CUDA code 
++ Python can easily understand Cython, and Cython can easily call C++ code
++ Minimum overhead
+
+Disadvantage
+
++ Need to know both C++ but this disadvantage is still there with PyCUDA but not with Continuum accelerate library 
+
 ## Environment
 + Windows 10
 + Python 3.4.3
--- benchmark.py
+++ benchmark.py
@@ -3,3 +3,61 @@
 """
 
 import numpy as np
+import unittest
+import sys
+import time
+
+# for compiling the cython code
+import pyximport
+pyximport.install(inplace=True,
+                  setup_args={"include_dirs": np.get_include()},
+                  reload_support=True)
+
+# Generate c and html file that shows respective c code
+from Cython.Build import cythonize
+
+# TODO: check with multi thread
+cythonize('Algo_RotateNN.pyx', annotate=True, nthreads=8)
+cythonize('Algo_RotateLinear.pyx', annotate=True, nthreads=8)
+cythonize('Algo_SubSampling.pyx', annotate=True, nthreads=8)
+
+# stream to hold log
+global custom_stream
+
+# variable to store ground truth
+global ground_truth
+
+
+# print utility
+def print_utility(time_taken):
+    global custom_stream
+    custom_stream.write('\nTime taken: ')
+    custom_stream.write(str(time_taken))
+    custom_stream.write('\n...........')
+    pass
+
+
+class TestRotateNN(unittest.TestCase):
+
+    def test_rotate_linear_interpolation(self):
+        start = time.time()
+        end = time.time()
+        print_utility(end - start)
+        pass
+
+    def test_rotate_linear_interpolation2(self):
+        start = time.time()
+        end = time.time()
+        print_utility(end - start)
+        pass
+
+if __name__ == '__main__':
+    global custom_stream
+
+    # To print on console or to file
+    custom_stream = sys.stdout
+    # custom_stream = open("report.txt", "w", encoding="utf-8")
+
+    # run suite
+    suite = unittest.TestLoader().loadTestsFromTestCase(TestRotateNN)
+    unittest.TextTestRunner(verbosity=3, stream=custom_stream).run(suite)
