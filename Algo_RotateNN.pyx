25d0c6dc00d020b10a5cdf10dd762f4da8c01edc
neuron-git-desktop <praveenneuron@gmail.com>
2016-03-16 17:42:58 +0100

    added code for plotting

--- Algo_RotateNN.pyx
+++ Algo_RotateNN.pyx
@@ -1,6 +1,15 @@
 
+cimport cython
+cimport numpy as np
+import numpy as np
+from cython cimport boundscheck, wraparound, initializedcheck, cdivision
+from libc.math cimport log
 
-class RotateNN():
-    pass
+@cython.boundscheck(False)
+@cython.wraparound(False)
+@cython.cdivision(True)
+@cython.initializedcheck(False)
+class AlgoRotateNN:
 
-
+    def cy_rotate_degree_0(self, image, theta):
+        pass
--- benchmark.py
+++ benchmark.py
@@ -6,7 +6,8 @@ import numpy as np
 import unittest
 import sys
 import time
-from scipy import ndimage
+from scipy import ndimage, misc
+import matplotlib.pyplot as plt
 
 # for compiling the cython code
 import pyximport
@@ -22,210 +23,125 @@ cythonize('Algo_RotateNN.pyx', annotate=True, nthreads=8)
 cythonize('Algo_RotateLinear.pyx', annotate=True, nthreads=8)
 cythonize('Algo_SubSampling.pyx', annotate=True, nthreads=8)
 
+from Algo_RotateNN import AlgoRotateNN
+
 # To print on console or to file
 custom_stream = sys.stdout
 # custom_stream = open("report.txt", "w", encoding="utf-8")
 
 # variables that store size of images
+lena_rgb_uint8 = ndimage.imread('lena.jpg')
+lena_rgb_float32 = lena_rgb_uint8.astype(np.float32)
+lena_grey_uint8 = ndimage.imread('lena.jpg', mode='L')
+lena_grey_float32 = lena_grey_uint8.astype(np.float32)
+
+
 small_shape_grey = (2048, 2048)
 large_shape_grey = (4096, 4096)
 small_shape_rgb = (2048, 2048, 3)
 large_shape_rgb = (4096, 4096, 3)
 
+misc.imresize(lena_rgb_float32, small_shape_grey, interp='bilinear')
+
+
+#plot image
+def plot_images(im0, im1, im2, im3, title):
+    fig = plt.figure()
+    fig.suptitle(title)
+
+    a = fig.add_subplot(2, 2, 1)
+    plt.imshow(im0)
+    a.set_title('Original')
+
+    a = fig.add_subplot(2, 2, 2)
+    plt.imshow(im1)
+    a.set_title('Numpy')
+
+    a = fig.add_subplot(2, 2, 3)
+    plt.imshow(im2)
+    a.set_title('Cython')
+
+    a = fig.add_subplot(2, 2, 4)
+    plt.imshow(im3)
+    a.set_title('CUDA')
+
+    plt.show()
+
+# get image utility
+def get_image(query):
+    args = query.split(sep='_')
+    lena = None
+
+    if args[2] == 'grey':
+        lena = ndimage.imread('lena.jpg', mode='L')
+    elif args[2] == 'rgb':
+        lena = ndimage.imread('lena.jpg')
+    else:
+        raise ValueError('Invalid color type. Allowed rgb or grey')
+
+    if args[3] == 'small':
+        lena = misc.imresize(lena, (2048, 2048), interp='bilinear')
+    elif args[3] == 'large':
+        lena = misc.imresize(lena, (4096, 4096), interp='bilinear')
+    else:
+        raise ValueError('Invalid size. Allowed small or large')
+
+    if args[4] == 'uint8':
+        lena = lena
+    elif args[4] == 'float32':
+        lena = lena.astype(np.float32)
+    else:
+        raise ValueError('Invalid size. Allowed uint8 or float32')
+
+    return lena
+
 
 # print utility
-def print_utility(time_taken_np, time_taken_cuda, message):
+def print_utility(time_taken_np, time_taken_cy, time_taken_cuda, message):
     global custom_stream
     # custom_stream.write('\n' + message)
-    custom_stream.write('\nTime taken numpy: ' + str(time_taken_np))
-    custom_stream.write('\nTime taken CUDA : ' + str(time_taken_cuda))
+    custom_stream.write('\nTime taken Numpy : ' + str(time_taken_np))
+    custom_stream.write('\nTime taken Cython: ' + str(time_taken_cy))
+    custom_stream.write('\nTime taken CUDA  : ' + str(time_taken_cuda))
     custom_stream.write('\n...........')
     pass
 
 
 class TestRotateNN(unittest.TestCase):
 
-    def test_grey_small_image_uint8(self):
+    def test_image_grey_small_uint8(self):
         theta = 33.7
-        test_images_grey = np.random.random_integers(0, 255, small_shape_grey).astype(np.uint8)
+        test_image = get_image(self._testMethodName)
 
         start = time.time()
-        img_dest1 = ndimage.rotate(test_images_grey, theta, order=0)
+        img_dest1 = ndimage.rotate(test_image, theta, order=0)
         end = time.time()
         t1 = end - start
 
         start = time.time()
-        img_dest2 = ndimage.rotate(test_images_grey, theta, order=0)
+        #img_dest2 = AlgoRotateNN().cy_rotate_degree_0(test_image, theta)
+        img_dest2 = ndimage.rotate(test_image, theta, order=0)
         end = time.time()
         t2 = end - start
 
-        print_utility(t1, t2, '')
+        start = time.time()
+        img_dest3 = ndimage.rotate(test_image, theta, order=0)
+        end = time.time()
+        t3 = end - start
+
+        print_utility(t1, t2, t3, '')
+
+        plot_images(test_image, img_dest1, img_dest2, img_dest3, self._testMethodName)
 
         # test case check
-        self.assertTrue(
-            np.allclose(img_dest1, img_dest2, rtol=1e-05, atol=1e-08, equal_nan=False),
-            'Results computed by GPU are not correct ...'
-        )
-        
-    def test_grey_small_image_float32(self):
-        theta = 33.7
-        test_images_grey = np.random.random_integers(0, 255, small_shape_grey).astype(np.float32)
-
-        start = time.time()
-        img_dest1 = ndimage.rotate(test_images_grey, theta, order=0)
-        end = time.time()
-        t1 = end - start
-
-        start = time.time()
-        img_dest2 = ndimage.rotate(test_images_grey, theta, order=0)
-        end = time.time()
-        t2 = end - start
-
-        print_utility(t1, t2, '')
-
-        # test case check
-        self.assertTrue(
-            np.allclose(img_dest1, img_dest2, rtol=1e-05, atol=1e-08, equal_nan=False),
-            'Results computed by GPU are not correct ...'
-        )
-
-    def test_grey_large_image_uint8(self):
-        theta = 33.7
-        test_images_grey = np.random.random_integers(0, 255, large_shape_grey).astype(np.uint8)
-
-        start = time.time()
-        img_dest1 = ndimage.rotate(test_images_grey, theta, order=0)
-        end = time.time()
-        t1 = end - start
-
-        start = time.time()
-        img_dest2 = ndimage.rotate(test_images_grey, theta, order=0)
-        end = time.time()
-        t2 = end - start
-
-        print_utility(t1, t2, '')
-
-        # test case check
-        self.assertTrue(
-            np.allclose(img_dest1, img_dest2, rtol=1e-05, atol=1e-08, equal_nan=False),
-            'Results computed by GPU are not correct ...'
-        )
-        
-    def test_grey_large_image_float32(self):
-        theta = 33.7
-        test_images_grey = np.random.random_integers(0, 255, large_shape_grey).astype(np.float32)
-
-        start = time.time()
-        img_dest1 = ndimage.rotate(test_images_grey, theta, order=0)
-        end = time.time()
-        t1 = end - start
-
-        start = time.time()
-        img_dest2 = ndimage.rotate(test_images_grey, theta, order=0)
-        end = time.time()
-        t2 = end - start
-
-        print_utility(t1, t2, '')
-
-        # test case check
-        self.assertTrue(
-            np.allclose(img_dest1, img_dest2, rtol=1e-05, atol=1e-08, equal_nan=False),
-            'Results computed by GPU are not correct ...'
-        )
-
-    def test_rgb_small_image_uint8(self):
-        theta = 33.7
-        test_images_rgb = np.random.random_integers(0, 255, small_shape_rgb).astype(np.uint8)
-
-        start = time.time()
-        img_dest1 = ndimage.rotate(test_images_rgb, theta, order=0)
-        end = time.time()
-        t1 = end - start
-
-        start = time.time()
-        img_dest2 = ndimage.rotate(test_images_rgb, theta, order=0)
-        end = time.time()
-        t2 = end - start
-
-        print_utility(t1, t2, '')
-
-        # test case check
-        self.assertTrue(
-            np.allclose(img_dest1, img_dest2, rtol=1e-05, atol=1e-08, equal_nan=False),
-            'Results computed by GPU are not correct ...'
-        )
-        
-    def test_rgb_small_image_float32(self):
-        theta = 33.7
-        test_images_rgb = np.random.random_integers(0, 255, small_shape_rgb).astype(np.float32)
-
-        start = time.time()
-        img_dest1 = ndimage.rotate(test_images_rgb, theta, order=0)
-        end = time.time()
-        t1 = end - start
-
-        start = time.time()
-        img_dest2 = ndimage.rotate(test_images_rgb, theta, order=0)
-        end = time.time()
-        t2 = end - start
-
-        print_utility(t1, t2, '')
-
-        # test case check
-        self.assertTrue(
-            np.allclose(img_dest1, img_dest2, rtol=1e-05, atol=1e-08, equal_nan=False),
-            'Results computed by GPU are not correct ...'
-        )
-
-    def test_rgb_large_image_uint8(self):
-        theta = 33.7
-        test_images_rgb = np.random.random_integers(0, 255, large_shape_rgb).astype(np.uint8)
-
-        start = time.time()
-        img_dest1 = ndimage.rotate(test_images_rgb, theta, order=0)
-        end = time.time()
-        t1 = end - start
-
-        start = time.time()
-        img_dest2 = ndimage.rotate(test_images_rgb, theta, order=0)
-        end = time.time()
-        t2 = end - start
-
-        print_utility(t1, t2, '')
-
-        # test case check
-        #self.assertTrue(
-        #    np.allclose(img_dest1, img_dest2, rtol=1e-05, atol=1e-08, equal_nan=False),
-        #    'Results computed by GPU are not correct ...'
-        #)
         pass
-        
-    def test_rgb_large_image_float32(self):
-        theta = 33.7
-        test_images_rgb = np.random.random_integers(0, 255, large_shape_rgb).astype(np.float32)
 
-        start = time.time()
-        img_dest1 = ndimage.rotate(test_images_rgb, theta, order=0)
-        end = time.time()
-        t1 = end - start
-
-        start = time.time()
-        img_dest2 = ndimage.rotate(test_images_rgb, theta, order=0)
-        end = time.time()
-        t2 = end - start
-
-        print_utility(t1, t2, '')
-
-        # test case check
-        #self.assertTrue(
-        #    np.allclose(img_dest1, img_dest2, rtol=1e-05, atol=1e-08, equal_nan=False),
-        #    'Results computed by GPU are not correct ...'
-        #)
-        pass
 
 if __name__ == '__main__':
 
     # run suite
-    custom_stream.write('\n\n\n *** Rotation of images (with NN-interpolation) *** \n\n')
+    custom_stream.write('\n----------------------------------------------------------------------\n')
+    custom_stream.write('\n          *** Rotation of images (with NN-interpolation) *** \n')
+    custom_stream.write('\n----------------------------------------------------------------------\n\n')
     suite = unittest.TestLoader().loadTestsFromTestCase(TestRotateNN)
     unittest.TextTestRunner(verbosity=3, stream=custom_stream).run(suite)
