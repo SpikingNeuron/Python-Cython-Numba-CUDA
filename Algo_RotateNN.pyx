0ef81b3ed298ce106ab7b4abe068c5758e4796be
neuron-git-desktop <praveenneuron@gmail.com>
2016-03-31 03:46:25 +0200

    updated rotate nn

--- Algo_RotateNN.pyx
+++ Algo_RotateNN.pyx
@@ -8,40 +8,6 @@ from cython import parallel
 from collections import namedtuple
 import sys
 from libc.math cimport sin, cos
-import numpy.testing as npt
-import gpuadder
-
-assert sizeof(int) == sizeof(np.int32_t)
-
-cdef extern from "Algo_RotateNN.hpp":
-    cdef cppclass C_GPUAdder "GPUAdder":
-        C_GPUAdder(np.int32_t*, int)
-        void increment()
-        void retreive()
-        void retreive_to(np.int32_t*, int)
-
-cdef class GPUAdder:
-    cdef C_GPUAdder* g
-    cdef int dim1
-
-    def __cinit__(self, np.ndarray[ndim=1, dtype=np.int32_t] arr):
-        self.dim1 = len(arr)
-        self.g = new C_GPUAdder(&arr[0], self.dim1)
-
-    def increment(self):
-        self.g.increment()
-
-    def retreive_inplace(self):
-        self.g.retreive()
-
-    def retreive(self):
-        cdef np.ndarray[ndim=1, dtype=np.int32_t] a = np.zeros(self.dim1, dtype=np.int32)
-
-        self.g.retreive_to(&a[0], self.dim1)
-
-        return a
-
-
 
 @cython.boundscheck(False)
 @cython.wraparound(False)
@@ -97,19 +63,63 @@ cdef class AlgoRotateNN:
             for index_y in range(oy):
                 index_y_new = <Py_ssize_t>((<np.float64_t>(index_x - cx)*sin_t + <np.float64_t>(index_y - cy)*cos_t) + ix/2)
                 index_x_new = <Py_ssize_t>((<np.float64_t>(index_x - cx)*cos_t - <np.float64_t>(index_y - cy)*sin_t) + iy/2)
-                if 0 <= index_x_new <= ix and 0 <= index_y_new <= iy:
+                if 0 <= index_x_new < ix and 0 <= index_y_new < iy:
                     dst[index_x, index_y] = src[index_x_new, index_y_new]
 
         return dst
 
 
-def test():
-    arr = np.array([1,2,2,2], dtype=np.int32)
-    adder = gpuadder.GPUAdder(arr)
-    adder.increment()
+    def cy_rotate_grey_float(self, image, theta_):
 
-    adder.retreive_inplace()
-    results2 = adder.retreive()
+        # storage variables
+        cdef np.float64_t mincx=0, mincy=0, maxcx=0, maxcy=0, coorx=0, coory=0
 
-    npt.assert_array_equal(arr, [2,3,3,3])
-    npt.assert_array_equal(results2, [2,3,3,3])
+        #
+        cdef np.float_t[:,:] src = image
+        cdef np.float64_t theta = np.pi / 180 * theta_
+        cdef np.float64_t cos_t = cos(theta)
+        cdef np.float64_t sin_t = sin(theta)
+        cdef Py_ssize_t ix = image.shape[0]
+        cdef Py_ssize_t iy = image.shape[1]
+        cdef np.float64_t sin_t_ix = sin_t * ix
+        cdef np.float64_t cos_t_ix = cos_t * ix
+        cdef np.float64_t sin_t_iy = sin_t * iy
+        cdef np.float64_t cos_t_iy = cos_t * iy
+
+        # find dimensions of new image
+        mincx = min(mincx, sin_t_ix)
+        maxcx = max(maxcx, sin_t_ix)
+        mincy = min(mincy, cos_t_ix)
+        maxcy = max(maxcy, cos_t_ix)
+        mincx = min(mincx, cos_t_iy)
+        maxcx = max(maxcx, cos_t_iy)
+        mincy = min(mincy, -sin_t_iy)
+        maxcy = max(maxcy, -sin_t_iy)
+        mincx = min(mincx, sin_t_ix + cos_t_iy)
+        maxcx = max(maxcx, sin_t_ix + cos_t_iy)
+        mincy = min(mincy, cos_t_ix - sin_t_iy)
+        maxcy = max(maxcy, cos_t_ix - sin_t_iy)
+        cdef Py_ssize_t oy = <Py_ssize_t>(maxcx - mincx + 0.5)
+        cdef Py_ssize_t ox = <Py_ssize_t>(maxcy - mincy + 0.5)
+
+        # create array for return
+        cdef np.float_t[:,:] dst = np.zeros((ox, oy), dtype=np.float)
+
+        # populate the destination image
+        cdef Py_ssize_t cx = ox/2
+        cdef Py_ssize_t cy = oy/2
+        cdef Py_ssize_t index_x, index_y, index_x_new, index_y_new
+
+        # rotation logic
+        cos_t = cos(-theta)
+        sin_t = sin(-theta)
+        for index_x in range(ox):
+            for index_y in range(oy):
+                index_y_new = <Py_ssize_t>((<np.float64_t>(index_x - cx)*sin_t + <np.float64_t>(index_y - cy)*cos_t) + ix/2)
+                index_x_new = <Py_ssize_t>((<np.float64_t>(index_x - cx)*cos_t - <np.float64_t>(index_y - cy)*sin_t) + iy/2)
+                if 0 <= index_x_new < ix and 0 <= index_y_new < iy:
+                    dst[index_x, index_y] = src[index_x_new, index_y_new]
+
+        return dst
+
+
--- benchmark.py
+++ benchmark.py
@@ -30,7 +30,8 @@ custom_stream = sys.stdout
 # custom_stream = open("report.txt", "w", encoding="utf-8")
 
 # config vars
-_PLOT = True
+_PLOT = False
+_ITER_NUM = 1
 
 
 def plot_images(im0, im1, im2, im3, title):
@@ -127,14 +128,17 @@ def print_utility(time_taken_np, time_taken_cy, time_taken_cuda, message):
     """
     global custom_stream
     # custom_stream.write('\n' + message)
-    custom_stream.write('\nTime taken Numpy : ' + str(time_taken_np))
-    custom_stream.write('\nTime taken Cython: ' + str(time_taken_cy))
-    custom_stream.write('\nTime taken CUDA  : ' + str(time_taken_cuda))
+    custom_stream.write('\nTime taken Numpy \t: ' + str(time_taken_np))
+    custom_stream.write('\nTime taken Cython\t: ' + str(time_taken_cy))
+    # custom_stream.write('\n\t\tSpeed up\t: ' + str(time_taken_np/time_taken_cy))
+    custom_stream.write('\nTime taken CUDA  \t: ' + str(time_taken_cuda))
+    # custom_stream.write('\n\t\tSpeed up\t: ' + str(time_taken_cuda))
+
     custom_stream.write('\n...........')
     pass
 
 
-class TestRotateNN(unittest.TestCase):
+class TestRotateNNGray(unittest.TestCase):
     """
     Unit test utility for rotating
     """
@@ -144,29 +148,163 @@ class TestRotateNN(unittest.TestCase):
         test_image = get_image(self._testMethodName)
 
         start = time.time()
-        img_dest1 = ndimage.rotate(test_image, theta, order=0)
+        for i in range(_ITER_NUM):
+            img_dest1 = ndimage.rotate(test_image, theta, order=0)
         end = time.time()
         t1 = end - start
 
         start = time.time()
-        img_dest2 = AlgoRotateNN().cy_rotate_grey_uint8(test_image, theta)
-        # img_dest2 = ndimage.rotate(test_image, theta, order=0)
+        for i in range(_ITER_NUM):
+            img_dest2 = AlgoRotateNN().cy_rotate_grey_uint8(test_image, theta)
         end = time.time()
         t2 = end - start
 
         start = time.time()
-        img_dest3 = ndimage.rotate(test_image, theta, order=0)
+        for i in range(_ITER_NUM):
+            # img_dest3 = ndimage.rotate(test_image, theta, order=0)
+            pass
         end = time.time()
         t3 = end - start
-
-        print('........')
-        print(test_image.shape)
-        print(img_dest1.shape)
-        print(img_dest2.shape)
+        t3 = 'Not available ....'
 
         print_utility(t1, t2, t3, '')
 
-        plot_images(test_image, img_dest1, img_dest2, img_dest3, self._testMethodName)
+        if _PLOT:
+            plot_images(test_image, img_dest1, img_dest2, img_dest2, self._testMethodName)
+
+        # test case check
+        pass
+
+    def test_image_grey_large_uint8(self):
+        theta = 33.33
+        test_image = get_image(self._testMethodName)
+
+        start = time.time()
+        for i in range(_ITER_NUM):
+            img_dest1 = ndimage.rotate(test_image, theta, order=0)
+        end = time.time()
+        t1 = end - start
+
+        start = time.time()
+        for i in range(_ITER_NUM):
+            img_dest2 = AlgoRotateNN().cy_rotate_grey_uint8(test_image, theta)
+        end = time.time()
+        t2 = end - start
+
+        start = time.time()
+        for i in range(_ITER_NUM):
+            # img_dest3 = ndimage.rotate(test_image, theta, order=0)
+            pass
+        end = time.time()
+        t3 = end - start
+        t3 = 'Not available ....'
+
+        print_utility(t1, t2, t3, '')
+
+        if _PLOT:
+            plot_images(test_image, img_dest1, img_dest2, img_dest2, self._testMethodName)
+
+        # test case check
+        pass
+
+    def test_image_grey_small_float(self):
+        theta = 33.33
+        test_image = get_image(self._testMethodName)
+
+        start = time.time()
+        for i in range(_ITER_NUM):
+            img_dest1 = ndimage.rotate(test_image, theta, order=0)
+        end = time.time()
+        t1 = end - start
+
+        start = time.time()
+        for i in range(_ITER_NUM):
+            img_dest2 = AlgoRotateNN().cy_rotate_grey_float(test_image, theta)
+        end = time.time()
+        t2 = end - start
+
+        start = time.time()
+        for i in range(_ITER_NUM):
+            # img_dest3 = ndimage.rotate(test_image, theta, order=0)
+            pass
+        end = time.time()
+        t3 = end - start
+        t3 = 'Not available ....'
+
+        print_utility(t1, t2, t3, '')
+
+        if _PLOT:
+            plot_images(test_image, img_dest1, img_dest2, img_dest2, self._testMethodName)
+
+        # test case check
+        pass
+
+    def test_image_grey_large_float(self):
+        theta = 33.33
+        test_image = get_image(self._testMethodName)
+
+        start = time.time()
+        for i in range(_ITER_NUM):
+            img_dest1 = ndimage.rotate(test_image, theta, order=0)
+        end = time.time()
+        t1 = end - start
+
+        start = time.time()
+        for i in range(_ITER_NUM):
+            img_dest2 = AlgoRotateNN().cy_rotate_grey_float(test_image, theta)
+        end = time.time()
+        t2 = end - start
+
+        start = time.time()
+        for i in range(_ITER_NUM):
+            # img_dest3 = ndimage.rotate(test_image, theta, order=0)
+            pass
+        end = time.time()
+        t3 = end - start
+        t3 = 'Not available ....'
+
+        print_utility(t1, t2, t3, '')
+
+        if _PLOT:
+            plot_images(test_image, img_dest1, img_dest2, img_dest2, self._testMethodName)
+
+        # test case check
+        pass
+
+
+class TestRotateNNRGB(unittest.TestCase):
+    """
+    Unit test utility for rotating
+    """
+
+    def test_image_rgb_small_uint8(self):
+        theta = 33.33
+        test_image = get_image(self._testMethodName)
+
+        start = time.time()
+        for i in range(_ITER_NUM):
+            img_dest1 = ndimage.rotate(test_image, theta, order=0)
+        end = time.time()
+        t1 = end - start
+
+        start = time.time()
+        for i in range(_ITER_NUM):
+            img_dest2 = AlgoRotateNN().cy_rotate_grey_uint8(test_image, theta)
+        end = time.time()
+        t2 = end - start
+
+        start = time.time()
+        for i in range(_ITER_NUM):
+            # img_dest3 = ndimage.rotate(test_image, theta, order=0)
+            pass
+        end = time.time()
+        t3 = end - start
+        t3 = 'Not available ....'
+
+        print_utility(t1, t2, t3, '')
+
+        if True:
+            plot_images(test_image, img_dest1, img_dest2, img_dest2, self._testMethodName)
 
         # test case check
         pass
@@ -176,7 +314,13 @@ if __name__ == '__main__':
 
     # run suite
     custom_stream.write('\n----------------------------------------------------------------------\n')
-    custom_stream.write('\n          *** Rotation of images (with NN-interpolation) ***          \n')
+    custom_stream.write('\n       *** Rotation of gray images (with NN-interpolation) ***        \n')
     custom_stream.write('\n----------------------------------------------------------------------\n\n')
-    suite = unittest.TestLoader().loadTestsFromTestCase(TestRotateNN)
+    suite = unittest.TestLoader().loadTestsFromTestCase(TestRotateNNGray)
     unittest.TextTestRunner(verbosity=3, stream=custom_stream).run(suite)
+    custom_stream.write('\n----------------------------------------------------------------------\n')
+    custom_stream.write('\n       *** Rotation of RGB images (with NN-interpolation) ***         \n')
+    custom_stream.write('\n----------------------------------------------------------------------\n\n')
+    suite = unittest.TestLoader().loadTestsFromTestCase(TestRotateNNRGB)
+    unittest.TextTestRunner(verbosity=3, stream=custom_stream).run(suite)
+
